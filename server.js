868e7710d43a162b3e964bad45e84368fd3f2dfb
anthonyChuks1 <anuebunwaanthony1@gmail.com>
2024-09-15 12:52:11 -0400

    added routing  for menuApi and more apis

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'docs')));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const userApiRoutes = require('./routes/users-api');
+const menuApiRoutes = require('./routes/menus-api');
 const widgetApiRoutes = require('./routes/widgets-api');
 const usersRoutes = require('./routes/users');
 
@@ -38,6 +39,7 @@ const usersRoutes = require('./routes/users');
 // Note: Feel free to replace the example routes below with your own
 // Note: Endpoints that return data (eg. JSON) usually start with `/api`
 app.use('/api/users', userApiRoutes);
+app.use('/api/menus', menuApiRoutes);
 app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
 // Note: mount other resources here, using the same pattern above

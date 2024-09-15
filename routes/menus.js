4cc8ac9570f6425e5d41d42f9059fa586a26441c
anthonyChuks1 <anuebunwaanthony1@gmail.com>
2024-09-15 13:26:31 -0400

    the menu routefor the api

---
+++ routes/menus.js
@@ -0,0 +1,32 @@
+/*
+ * All routes for Users are defined here
+ * Since this file is loaded in server.js into /users,
+ *   these routes are mounted onto /users
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router  = express.Router();
+
+router.get('/', (req, res) => {
+  res.render('menus');
+}); 
+
+
+
+// router.post('/user', (req, res)=> {
+  
+// });
+
+// router.post ('/order', (req,res) => {
+
+// });
+
+// router.get('/order:phonenumber', (req,res) => {
+
+// });
+
+// router.get('/admin', (req, res) => {
+
+// });
+module.exports = router;

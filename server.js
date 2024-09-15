f4db92672bd0d867003d4c434cf54da97327cf89
anthonyChuks1 <anuebunwaanthony1@gmail.com>
2024-09-15 13:26:07 -0400

    added menu api route for ajax

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ const userApiRoutes = require('./routes/users-api');
 const menuApiRoutes = require('./routes/menus-api');
 const widgetApiRoutes = require('./routes/widgets-api');
 const usersRoutes = require('./routes/users');
+const menusRoutes = require('./routes/menus');
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -42,6 +43,7 @@ app.use('/api/users', userApiRoutes);
 app.use('/api/menus', menuApiRoutes);
 app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
+app.use('/menus', menusRoutes);
 // Note: mount other resources here, using the same pattern above
 
 // Home page

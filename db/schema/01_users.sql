e9038013f5a2dc1901953fafeb2e407cc17b931b
anthonyChuks1 <anuebunwaanthony1@gmail.com>
2024-09-16 19:33:37 -0400

    changed the schematics of the table by removing the order_items table

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -4,8 +4,6 @@ DROP TABLE IF EXISTS users CASCADE;
 
 DROP TABLE IF EXISTS menu_items CASCADE;
 
-DROP TABLE IF EXISTS order_items CASCADE;
-
 DROP TABLE IF EXISTS orders CASCADE;
 
 CREATE TABLE
@@ -25,17 +23,11 @@ CREATE TABLE
   );
 
 CREATE TABLE
-  order_items (
-    id SERIAL PRIMARY KEY NOT NULL,
-    menu_item_id INTEGER REFERENCES menu_items (id) ON DELETE CASCADE,
-    quantity INTEGER NOT NULL DEFAULT 0
-  );
-
-CREATE TABLE
   orders (
     id SERIAL PRIMARY KEY NOT NULL,
     user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-    order_item_id INTEGER REFERENCES order_items(id) ON DELETE CASCADE,
     order_date DATE NOT NULL,
+    quantity INTEGER NOT NULL DEFAULT 0,
+    menu_item_id INTEGER REFERENCES menu_items (id) ON DELETE CASCADE,
     status VARCHAR(10) NOT NULL
   );

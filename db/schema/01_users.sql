ca20e3e76df37d0db98155ba747a185d33899894
anthonyChuks1 <anuebunwaanthony1@gmail.com>
2024-09-17 21:43:28 -0400

    changed the schema by adding order_items and adjusting orders table

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,8 +1,9 @@
-
 -- Drop and recreate Users table (Example)
 DROP TABLE IF EXISTS users CASCADE;
 
 DROP TABLE IF EXISTS menu_items CASCADE;
+
+DROP TABLE IF EXISTS order_items CASCADE;
 
 DROP TABLE IF EXISTS orders CASCADE;
 
@@ -22,12 +23,20 @@ CREATE TABLE
     image VARCHAR(255) NOT NULL
   );
 
+
+
 CREATE TABLE
   orders (
     id SERIAL PRIMARY KEY NOT NULL,
-    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    user_id INTEGER REFERENCES users (id) ON DELETE CASCADE,
     order_date DATE NOT NULL,
-    quantity INTEGER NOT NULL DEFAULT 0,
-    menu_item_id INTEGER REFERENCES menu_items (id) ON DELETE CASCADE,
     status VARCHAR(10) NOT NULL
+  );
+
+  CREATE TABLE
+  order_items (
+    id SERIAL PRIMARY KEY NOT NULL,
+    menu_item_id INTEGER REFERENCES menu_items (id) ON DELETE CASCADE,
+    order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
+    quantity INTEGER NOT NULL DEFAULT 0
   );

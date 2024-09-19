f90ff813c9784cabaf0a9c619ad19ec7fa07cd09
anthonyChuks1 <anuebunwaanthony1@gmail.com>
2024-09-19 17:12:09 -0400

    update for button counter

--- public/scripts/buttonCounter.js
+++ public/scripts/buttonCounter.js
@@ -1,59 +1,123 @@
 $(document).ready(function () {
-  //console.log("Making sure it is on this line ->");
+  const reviewWindow = document.getElementById("reviewPopup");
+  const changesBtn = document.getElementById("makeChangesBtn");
+
+  const span = document.getElementsByClassName("close")[0];
+  const confirmBtn = document.getElementById("confirmOrderBtn");
+  const selectedItems = {};
+
+  function updateSelectedItems(foodBox, quantity) {
+    const foodName = foodBox.find("label").text().trim();
+    if (quantity > 0) {
+      selectedItems[foodName] = quantity;
+    } else {
+      delete selectedItems[foodName];
+    }
+  }
+
+  $("#reviewOrderBtn").on("click", function () {
+    const orderItemsContainer = $("#orderItems");
+    orderItemsContainer.empty();
+    for (const [name, quantity] of Object.entries(selectedItems)) {
+      const itemElement = $("<div>").text(`${name} (${quantity})`);
+      orderItemsContainer.append(itemElement);
+    }
+    $("#reviewPopup").show();
+  });
+
+  $(".close").on("click", function () {
+    $("#reviewPopup").hide();
+  });
+
+  // Clicking the X
+  span.onclick = function () {
+    // console.log('button clicked')
+    reviewWindow.style.display = "none";
+  };
+
+  // Clicking the 'make changes'
+  changesBtn.onclick = function () {
+    // console.log('button clicked')
+    reviewWindow.style.display = "none";
+  };
+
+  // Clicking the 'confirm'
+  confirmBtn.onclick = function () {};
+
   const menuContainer = $("#menu_container");
 
   // Code edited to hold an object containind the items that have been selected!
 
-  const selectedItems = {};
-
   console.log("Menu container:", menuContainer);
 
-  function handleIncrement() {
-    const $button = $(this);
-    const foodBox = $button.closest(".food-box");
-    const counter = foodBox.find("#counter-value");
-    let currentVal = parseInt(counter.text()) || 0;
-
-    currentVal += 1;
-
-    counter.text(currentVal);
-    //console.log("Increment");
+  let currentTotal = 0;
+function handleIncrement() {
+  const $button = $(this);
+  const foodBox = $button.closest(".food-box");
+  const counter = foodBox.find("#counter-value");
+  let currentVal = parseInt(counter.text());
+  currentVal += 1;
+  currentItemTotal += 1;
+  counter.text(currentVal);
+  updateSelectedItems(foodBox, currentVal);
+  const itemTotalCounter = $(".nav-right-label").children("counter");
+  itemTotalCounter.text(`Make Item Counter Here (${currentItemTotal} Items)`);
+}
+function handleDecrement() {
+  const $button = $(this);
+  const foodBox = $button.closest(".food-box");
+  const counter = foodBox.find("#counter-value");
+  let currentVal = parseInt(counter.text());
+  if (currentVal > 0) {
+    currentVal -= 1;
+    currentItemTotal -= 1;
   }
-
-  function handleDecrement() {
-    const $button = $(this);
-    const foodBox = $button.closest(".food-box");
-    const counter = foodBox.find("#counter-value");
-    let currentVal = parseInt(counter.text()) || 0;
-    if (currentVal > 0) {
-      currentVal -= 1;
-    }
-
-    counter.text(currentVal);
-    //console.log("Updated current value:", currentVal);
-  }
-
-  
+  counter.text(currentVal);
+  updateSelectedItems(foodBox, currentVal);
+  const itemTotalCounter = $(".nav-right-label").children("counter");
+  itemTotalCounter.text(`Make Item Counter Here (${currentItemTotal} Items)`);
+}
 
   menuContainer.on("click", ".increment-btn", handleIncrement);
   menuContainer.on("click", ".decrement-btn", handleDecrement);
 
-  $('confirmOrderBtn').on('submit', function(event){
+  console.log($("#confirmOrderBtn"));
+
+  $("#confirmOrderBtn").on("click", function (event) {
     event.preventDefault();
     const $button = $(this);
-    const foodBox = $button.closest(".food-box");
-    const counter = foodBox.find("#counter-value");
+    const orderItems = $("#orderItems").children();
+    const orderItemsArray = [];
+    const phoneNumber = $("#phoneNumber").val();
+    for (const item of orderItems) {
+      const itemValues = $(item).text().split("$");
+      const [price, quantity] = itemValues[1].split("(");
+      const name = itemValues[0].trim();
+      const adjustedPrice = price.trim();
+      const adjustedQuantity = quantity.replace(")", "").trim();
+      console.log(name, adjustedPrice, adjustedQuantity);
+      orderItemsArray.push({ name, quantity: adjustedQuantity });
+    }
+    // const counter = foodBox.find("#counter-value");
+    console.log($button);
+    console.log(orderItems);
 
-  
-    // $.ajax({
-    //   type: "POST",
-    //   url: "/order",
-    //   data: 
-    // })
-    
-  })
-  
-})
+    const data = {
+      orders: orderItemsArray,
+      phoneNumber,
+    };
+
+    console.log(data);
+
+    // console.log(counter)
+    $.ajax({
+      type: "POST",
+      url: "/order",
+      contentType: "application/json",
+      data: JSON.stringify(data),
+    });
+  });
+});
 
 // Pseudocode for updating the Review Order List
 
@@ -74,3 +138,30 @@ $(document).ready(function () {
 // Close Popup:
 // When the close button in the popup is clicked:
 // Hide the popup.
+let currentTotal = 0;
+function handleIncrement() {
+  const $button = $(this);
+  const foodBox = $button.closest(".food-box");
+  const counter = foodBox.find("#counter-value");
+  let currentVal = parseInt(counter.text());
+  currentVal += 1;
+  currentItemTotal += 1;
+  counter.text(currentVal);
+  updateSelectedItems(foodBox, currentVal);
+  const itemTotalCounter = $(".nav-right-label").children("counter");
+  itemTotalCounter.text(`Make Item Counter Here (${currentItemTotal} Items)`);
+}
+function handleDecrement() {
+  const $button = $(this);
+  const foodBox = $button.closest(".food-box");
+  const counter = foodBox.find("#counter-value");
+  let currentVal = parseInt(counter.text());
+  if (currentVal > 0) {
+    currentVal -= 1;
+    currentItemTotal -= 1;
+  }
+  counter.text(currentVal);
+  updateSelectedItems(foodBox, currentVal);
+  const itemTotalCounter = $(".nav-right-label").children("counter");
+  itemTotalCounter.text(`Make Item Counter Here (${currentItemTotal} Items)`);
+}
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,22 +1,38 @@
 /*
- * All routes for Users are defined here
- * Since this file is loaded in server.js into /users,
- *   these routes are mounted onto /users
+ * All routes for Admin are defined here
+ * Since this file is loaded in server.js into /admin,
+ *   these routes are mounted onto /admin
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
 const express = require('express');
 const router  = express.Router();
 
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Or, for ESM: import twilio from "twilio";
+
+// Find your Account SID and Auth Token at twilio.com/console
+// and set the environment variables. See http://twil.io/secure
+// const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// const authToken = process.env.TWILIO_AUTH_TOKEN;
+// const client = require('twilio')(accountSid, authToken);
+
+// const createMessage = () => {
+//   client.messages.create({
+//     body: "This is the ship that made the Kessel Run in fourteen parsecs?",
+//     from: "+13437778496",
+//     to: "+13437778496",
+//   }).then((message) => console.log(message.body));
+// }
+
 router.get('/admin', (req, res) => {
+  //createMessage();
   res.render('admin');
-})
-//To:Do add status changer will req status from the form submit
-router.post('/admin', (req, res) =>{
+});
 
-})
-
-//route 
-
+// ToDo: add status changer will req status from the form submit
+router.post('/admin', (req, res) => {
+  res.send('Status change functionality to be implemented');
+});
 
 module.exports = router;

48ca1e8f6b5685cf0f55f2e9bf8d73482149db32
anthonyChuks1 <anuebunwaanthony1@gmail.com>
2024-09-18 23:45:09 -0400

    Implemented the button counter

--- public/scripts/buttonCounter.js
+++ public/scripts/buttonCounter.js
@@ -1,32 +1,32 @@
 $(document).ready(function () {
-  console.log("Making sure it is on this line ->");
+  //console.log("Making sure it is on this line ->");
   const menuContainer = $("#menu_container");
 
-  console.log("Menu container:", menuContainer);
+  //console.log("Menu container:", menuContainer);
 
   function handleIncrement() {
     const $button = $(this);
     const foodBox = $button.closest(".food-box");
     const counter = foodBox.find("#counter-value");
-    let currentVal = parseInt(counter.text(), 10) || 0;
+    let currentVal = parseInt(counter.text()) || 0;
 
     currentVal += 1;
 
     counter.text(currentVal);
-    console.log("Increment");
+    //console.log("Increment");
   }
 
   function handleDecrement() {
     const $button = $(this);
     const foodBox = $button.closest(".food-box");
     const counter = foodBox.find("#counter-value");
-    let currentVal = parseInt(counter.text(), 10) || 0;
+    let currentVal = parseInt(counter.text()) || 0;
     if (currentVal > 0) {
       currentVal -= 1;
     }
 
     counter.text(currentVal);
-    console.log("Updated current value:", currentVal);
+    //console.log("Updated current value:", currentVal);
   }
 
   menuContainer.on("click", ".increment-btn", handleIncrement);

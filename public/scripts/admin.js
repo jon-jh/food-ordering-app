74e76eb3aca199034b16ac2a9adf26fcc503b759
anthonyChuks1 <anuebunwaanthony1@gmail.com>
2024-09-19 13:32:13 -0400

    added more buttons

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -15,12 +15,9 @@
  * @throws Will alert an error message if the AJAX request fails.
  */
 
-
-
 //All for admin page
-const $orderContainer = $('#order-container');
+const $orderContainer = $("#order-container");
 const createOrder = (order) => {
-
   return $(`
     <div class="order">
       <div class="order-header">
@@ -33,54 +30,53 @@ const createOrder = (order) => {
       </div>
       <div class="order-footer">
       <p>User ID: ${order.user_id}</p>
-        <img src="${order.image}" alt="${order.name}" style="width: 120px; height: auto;"><p class=status>status: ${order.status}</p>
+        <img src="${order.image}" alt="${
+    order.name
+  }" style="width: 120px; height: auto;"><p class=status>status: ${
+    order.status
+  }</p>
         <form class="order-form">
     <button type="submit" id="confirm-10">Confirm - 10 Minutes</button>
     <button type="submit" id="confirm-20">Confirm - 20 Minutes</button>
     <button type="submit" id="confirm-30">Confirm - 30 Minutes</button>
-    <button type="submit" class= "btn btn-success"  id="complete-order">Complete Order</button>
-    <button type="submit" class= "btn btn-danger"  id="cancel-order">Cancel Order</button>
+    <button type="submit" class= "btn btn-success" id="complete-order">Complete Order</button>
+    <button type="submit" class= "btn btn-danger" id="cancel-order">Cancel Order</button>
     </form>
       </div>
     </div>
   `);
 };
 
-
-
 const renderOrders = (arrayOfOrders) => {
   let $order;
-  console.log('Rendering pending orders: ');
+  console.log("Rendering pending orders: ");
   for (const order of arrayOfOrders) {
-    if(order.status === 'pending') {
-     $order = createOrder(order);
-    $orderContainer.prepend($order);
+    if (order.status === "pending") {
+      $order = createOrder(order);
+      $orderContainer.prepend($order);
     }
   }
 
-  console.log('Rendering processing orders: ');
+  console.log("Rendering processing orders: ");
   for (const order of arrayOfOrders) {
-    if(order.status === 'processing') {
-     $order = createOrder(order);
-    $orderContainer.prepend($order);
+    if (order.status === "processing") {
+      $order = createOrder(order);
+      $orderContainer.prepend($order);
     }
   }
-
-
-}
-const loadOrders = function() {
-  
+};
+const loadOrders = function () {
   $.ajax({
-    method: 'GET',
-    url: '/api/orders',
+    method: "GET",
+    url: "/api/orders",
     success: (serverResponseOrders) => {
-      console.log('Orders loaded:', serverResponseOrders);
+      console.log("Orders loaded:", serverResponseOrders);
       $orderContainer.empty();
       renderOrders(serverResponseOrders.orders);
     },
     error: (jqXHR, textStatus, errorThrown) => {
-      console.error('Error loading orders:', textStatus, errorThrown);
-    }
+      console.error("Error loading orders:", textStatus, errorThrown);
+    },
   });
 };
 
